126d448b71ef1f839a8d924bed07fe29a98d7df3
Jorge Acereda <jacereda@gmail.com>
2012-11-08 22:43:37 +0100

    GLTERM

--- examples/cvtest.go
+++ examples/cvtest.go
@@ -8,6 +8,8 @@ import (
 
 func handler(e *cv.Event) uintptr {
 	switch e.Type() {
+	default:
+		return 0
 	case cv.HINT_NAME:
 		return cv.String("cvtest")
 	case cv.HINT_BORDERS:
@@ -22,38 +24,30 @@ func handler(e *cv.Event) uintptr {
 		return 640
 	case cv.ON_INIT:
 		log.Println("INIT")
-		return 1
 	case cv.ON_TERM:
 		log.Println("TERM")
-		return 1
 	case cv.ON_GLINIT:
 		log.Println("GLINIT")
 		gl.Init()
-		return 1
+	case cv.ON_GLTERM:
+		log.Println("GLTERM")
 	case cv.ON_DOWN:
 		log.Println("DOWN", cv.KeyName(e.Which()))
-		return 1
 	case cv.ON_UP:
 		log.Println("UP", cv.KeyName(e.Which()))
-		return 1
 	case cv.ON_UNICODE:
 		log.Printf("UNICODE %c\n", e.Unicode())
-		return 1
 	case cv.ON_MOTION:
 		log.Println("MOTION", e.X(), e.Y())
-		return 1
 	case cv.ON_CLOSE:
 		log.Println("CLOSE")
 		cv.Quit()
-		return 1
 	case cv.ON_RESIZE:
 		log.Println("RESIZE", e.Width(), e.Height())
-		return 1
 	case cv.ON_UPDATE:
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		return 1
 	}
-	return 0
+	return 1
 }
 
 func main() {
